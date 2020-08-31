36722dbf4b2aec2b8ab2d497e944191871a2d2d8
joeluca24 <65137591+joeluca24@users.noreply.github.com>
2020-08-30 21:03:44 -0400

    udpdated burgers_contollers.js and burgers.js

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,6 +1,6 @@
-var express = require("express");
+const express = require("express");
 
-var router = express.Router();
+const router = express.Router();
 
 
 var burger = require("../models/burger.js");
@@ -12,21 +12,50 @@ router.get("/", function (req, res) {
                 burgers: data
             };
             console.log(hbsObject);
-            
-              res.render("index", hbsObject);
+
+            res.render("index", hbsObject);
         })
         .catch(err => {
             console.log(err);
         });
 });
+router.post("/api/burgers", function (req, res) {
+    burger.create([
+        "name", "devoured"
+    ], [
+        req.body.name, req.body.devoured
+    ], function (result) {
+        res.json({ id: result.insertId });
+    });
+});
 
+router.put("/api/burgers/:id", function (req, res) {
+    var condition = "id = " + req.params.id;
 
+    console.log("condition", condition);
 
+    burger.update({
+        devoured: req.body.devoured
+    }, condition, function (result) {
+        if (result.changedRows == 0) {
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    });
+});
+router.delete("/api/burgers/:id", function (req, res) {
+    var condition = "id = " + req.params.id;
 
+    burger.delete(condition, function (result) {
+        if (result.affectedRows == 0) {
 
-
-
-
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    });
+});
 
 
 
--- models/burger.js
+++ models/burger.js
@@ -1,29 +1,29 @@
- const orm = require("../config/orm.js");
- 
+const orm = require("../config/orm.js");
 
- const burger = {
+
+const burger = {
     read: () => orm.selectAll(),
-    create: (cols,vals) => orm.insertOne(cols,vals),
+    create: (cols, vals) => orm.insertOne(cols, vals),
     update: (objcolsvals, condition) => orm.updateOne(objcolsvals, condition)
- }
- all: function(cb) {
-    orm.all("burgers", function(res) {
+}
+all: function(cb) {
+    orm.all("burgers", function (res) {
         cb(res);
     });
 },
 
 create: function(cols, vals, cb) {
-    orm.create("burgers", cols, vals, function(res) {
+    orm.create("burgers", cols, vals, function (res) {
         cb(res);
     });
 },
 update: function(objColVals, condition, cb) {
-    orm.update("burgers", objColVals, condition, function(res) {
+    orm.update("burgers", objColVals, condition, function (res) {
         cb(res);
     });
 },
 delete: function(condition, cb) {
-    orm.delete("burgers", condition, function(res) {
+    orm.delete("burgers", condition, function (res) {
         cb(res);
     });
 }
