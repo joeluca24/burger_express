4a4dc84ac2125a49ba66ecb4a0ae62acedd3ca44
joeluca24 <65137591+joeluca24@users.noreply.github.com>
2020-08-30 20:39:16 -0400

    updated functions burgers.js

--- config/connection.js
+++ config/connection.js
@@ -5,7 +5,8 @@ var connection = mysql.createConnection({
   port: 3306,
   user: "root",
   password: "root24",
-  database: "burgers_db"
+  database: "burgers_db"3
+  
 });
 
 // Make connection.
--- models/burger.js
+++ models/burger.js
@@ -1,22 +1,32 @@
-var orm = require("../config/orm.js");
+ const orm = require("../config/orm.js");
  
 
-var burger = {
+ const burger = {
     read: () => orm.selectAll(),
     create: (cols,vals) => orm.insertOne(cols,vals),
     update: (objcolsvals, condition) => orm.updateOne(objcolsvals, condition)
  }
+ all: function(cb) {
+    orm.all("burgers", function(res) {
+        cb(res);
+    });
+},
 
-
-
-
-
-
-
-
-
-
-
+create: function(cols, vals, cb) {
+    orm.create("burgers", cols, vals, function(res) {
+        cb(res);
+    });
+},
+update: function(objColVals, condition, cb) {
+    orm.update("burgers", objColVals, condition, function(res) {
+        cb(res);
+    });
+},
+delete: function(condition, cb) {
+    orm.delete("burgers", condition, function(res) {
+        cb(res);
+    });
+}
 
 
 
