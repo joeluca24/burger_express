f8fb9ac6356a17a4dec5393366825cd01d371c35
joeluca24 <65137591+joeluca24@users.noreply.github.com>
2020-09-04 18:44:56 -0400

    fixed connection err

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,19 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "root24",
-  database: "burgers_db"
-  
-});
+var connection;
+if(typeof process.env.JAWSDB_URL === 'undefined'){
+ connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "root24",
+    database: "burgers_db"
+    
+  });
+}
+else{
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
 
 // Make connection.
 connection.connect(function(err) {
